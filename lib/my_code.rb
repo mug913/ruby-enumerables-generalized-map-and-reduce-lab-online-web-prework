70b1c18b8da948fdbe2eab73f2240a735f04810c
Brad Alexander <brad.alexander913@gmail.com>
2019-08-11 18:05:34 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,20 @@
-# Your Code Here
+def map(array)
+  i = 0
+  arr = []
+    while i < array.length
+      arr.push(yield(array[i]))
+      i += 1 
+    end
+  return arr
+end
+
+def reduce(array, i=0)
+  x = 0
+    while i < array.length
+      
+      
+      
+      i += 1 
+    end
+  return x
+end
